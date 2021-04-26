50b8c89225631e96347d02c63a8f2487d6cd6650
Aleksndr Illyashenko <alex1002x@gmail.com>
2021-04-26 22:03:52 +0300

    feat

--- src/components/content-converter/content-converter.js
+++ src/components/content-converter/content-converter.js
@@ -1,169 +1,159 @@
-import { useEffect, useState, useCallback } from 'react';
-import { Spinner } from 'reactstrap';
-import { useSelector, useDispatch } from 'react-redux';
-import { Label, Input, FormGroup, FormFeedback } from 'reactstrap';
+import { useEffect, useState, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Label, Input, FormGroup, FormFeedback, Spinner } from "reactstrap";
 
-import { writeToLocalStorige, readFromLocalStorige } from "../../utils";
-import { getList, getIsLoading, request } from '../../ducks/converter';
-import { getConversion } from './helpers';
+import {
+  getList,
+  getIsLoading,
+  getItemBySelectCurrencyCode,
+  getItemByChangeableCurrencyCode,
+  request,
+  setChangeableCurrencyCode,
+  setSelectCurrencyCode,
+  swapCurrencyCodeCallback,
+} from "../../ducks/converter";
+import { getConversion } from "./helpers";
 
-import './content-converter.css';
-import imgAction from './seeds/convert-image.png';
+import "./content-converter.css";
+import imgAction from "./seeds/convert-image.png";
 
 const ContentConverter = () => {
-    const DEFAULT_CHANGEABLE_CURRENCY_CODE = 'UAH';
-    const DEFAULT_SELECT_CURRENCY_CODE = 'USD';
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const list = useSelector(getList);
+  const itemBySelectCurrencyCode = useSelector(getItemBySelectCurrencyCode);
+  const itemByChangeableCurrencyCode = useSelector(
+    getItemByChangeableCurrencyCode,
+  );
 
-    const isLoading = useSelector(getIsLoading);
-    const list = useSelector(getList);
+  useEffect(() => {
+    if (list.length < 1) {
+      dispatch(request());
+    }
+  }, [dispatch, list]);
 
-    useEffect(() => {
+  const [changeableCurrencyAmount, setChangeableCurrencyAmount] = useState(100);
 
-        if (!list || list.length < 1) {
-            dispatch(request());
-        }
-    }, [dispatch, list]);
+  const handleChangeableCurrencyChange = useCallback((e) => {
+    setChangeableCurrencyAmount(e.target.value);
+  }, []);
 
-    // states
+  const handleChangeableCurrencyCodeChange = useCallback(
+    (e) => {
+      dispatch(setChangeableCurrencyCode(e.target.value));
+    },
+    [dispatch],
+  );
 
-    const [changeableCurrencyAmount, setChangeableCurrencyAmount] = useState(100);
-    const [changeableCurrencyCode, setChangeableCurrencyCode] = useState('');
-    const [selectCurrencyCode, setSelectCurrencyCodeCodeChange] = useState('');
+  const handleSelectCurrencyCodeCodeChange = useCallback(
+    (e) => {
+      dispatch(setSelectCurrencyCode(e.target.value));
+    },
+    [dispatch],
+  );
 
-    // useEffects
+  const getSelectCurrencyValue = () =>
+    getConversion(
+      itemBySelectCurrencyCode,
+      itemByChangeableCurrencyCode,
+      changeableCurrencyAmount,
+    );
 
-    useEffect(() => {
-        const currentChangeableCurrencyCode = readFromLocalStorige('changeable-currency_code') || DEFAULT_CHANGEABLE_CURRENCY_CODE
+  const swapCode = useCallback(() => {
+    dispatch(swapCurrencyCodeCallback());
+  }, [dispatch]);
 
-        const isExist = list.find(item => item.code === currentChangeableCurrencyCode)
-        if (isExist && !changeableCurrencyCode) {
-            setChangeableCurrencyCode(isExist.code)
-        }
-    }, [list, changeableCurrencyCode]);
+  if (!itemBySelectCurrencyCode || !itemByChangeableCurrencyCode) {
+    return null;
+  }
 
-    useEffect(() => {
-        const currentSelectCurrencyCode = readFromLocalStorige('select-currency_code') || DEFAULT_SELECT_CURRENCY_CODE
-
-        const isExist = list.find(item => item.code === currentSelectCurrencyCode)
-        if (isExist && !selectCurrencyCode) {
-            setSelectCurrencyCodeCodeChange(isExist.code)
-        }
-    }, [list, selectCurrencyCode]);
-
-    // handleChanges
-
-    const handleChangeableCurrencyChange = useCallback(e => { setChangeableCurrencyAmount(e.target.value) }, []);
-
-    const handleChangeableCurrencyCodeChange = useCallback(e => {
-        setChangeableCurrencyCode(e.target.value);
-
-        writeToLocalStorige('changeable-currency_code', e.target.value);
-    }, []);
-
-    const handleSelectCurrencyCodeCodeChange = useCallback(e => {
-        setSelectCurrencyCodeCodeChange(e.target.value);
-
-        writeToLocalStorige('select-currency_code', e.target.value);
-    }, []);
-
-    // utils
-
-    const getSelectCurrencyValue = () => getConversion(list, changeableCurrencyCode, selectCurrencyCode, changeableCurrencyAmount);
-
-    const swapCode = () => {
-        const _selectCurrencyCode = selectCurrencyCode;
-        const _changeableCurrencyCode = changeableCurrencyCode;
-
-        setChangeableCurrencyCode(_selectCurrencyCode);
-        setSelectCurrencyCodeCodeChange(_changeableCurrencyCode);
-
-        writeToLocalStorige('changeable-currency_code', _selectCurrencyCode);
-        writeToLocalStorige('select-currency_code', _changeableCurrencyCode);
-    };
-
-    return (
-        <div className="ContentConverter">
-            {
-                isLoading &&
-
-                <div className="ContentConverter-loader">
-                    <Spinner color="warning" size="64px" />
-                </div>
-            }
-
-            <div className="ContentConverter-grid">
-                <div className="ContentConverter-gridInputItem">
-                    <FormGroup>
-                        <Label for="changeableCurrency">Меняю</Label>
-                        <Input
-                            type="number"
-                            name="number"
-                            id="changeableCurrency"
-                            value={changeableCurrencyAmount}
-                            onChange={handleChangeableCurrencyChange}
-                            invalid={!changeableCurrencyAmount}
-                        />
-                        <FormFeedback>Введите количество валюты</FormFeedback>
-                    </FormGroup>
-                </div>
-
-                <div className="ContentConverter-gridSelectItem">
-                    <FormGroup>
-                        <Label for="changeableCurrencyCode">Валюта</Label>
-                        <Input
-                            type="select"
-                            name="select"
-                            id="changeableCurrencyCode"
-                            value={changeableCurrencyCode}
-                            onChange={handleChangeableCurrencyCodeChange}
-                        >
-                            {
-                                list.map(({ code }) => <option key={code}>{code}</option>)
-                            }
-                        </Input>
-                    </FormGroup>
-                </div>
-
-                <div className="ContentConverter-gridAction">
-                    <button className="ContentConverter-gridActionButton" onClick={swapCode}>
-                        <img src={imgAction} alt="ContentConverter-imgAction" className="ContentConverter-image" />
-                    </button>
-                </div>
-
-                <div className="ContentConverter-gridInputItem">
-                    <FormGroup>
-                        <Label for="selectCurrency">Получаю</Label>
-                        <Input
-                            type="number"
-                            name="number"
-                            id="selectCurrency"
-                            value={getSelectCurrencyValue()}
-                            disabled
-                        />
-                    </FormGroup>
-                </div>
-
-                <div className="ContentConverter-gridSelectItem">
-                    <FormGroup>
-                        <Label for="selcetCurrencyCode">Валюта</Label>
-                        <Input
-                            type="select"
-                            name="select"
-                            id="selcetCurrencyCode"
-                            value={selectCurrencyCode}
-                            onChange={handleSelectCurrencyCodeCodeChange}
-                        >
-                            {
-                                list.map(({ code }) => <option key={code}>{code}</option>)
-                            }
-                        </Input>
-                    </FormGroup>
-                </div>
-            </div>
+  return (
+    <div className="ContentConverter">
+      {isLoading && (
+        <div className="ContentConverter-loader">
+          <Spinner color="warning" size="64px" />
         </div>
-    )
-}
+      )}
+
+      <div className="ContentConverter-grid">
+        <div className="ContentConverter-gridInputItem">
+          <FormGroup>
+            <Label for="changeableCurrency">Меняю</Label>
+            <Input
+              type="number"
+              name="number"
+              id="changeableCurrency"
+              value={changeableCurrencyAmount}
+              onChange={handleChangeableCurrencyChange}
+              invalid={!changeableCurrencyAmount}
+            />
+            <FormFeedback>Введите количество валюты</FormFeedback>
+          </FormGroup>
+        </div>
+
+        <div className="ContentConverter-gridSelectItem">
+          <FormGroup>
+            <Label for="changeableCurrencyCode">Валюта</Label>
+            <Input
+              type="select"
+              name="select"
+              id="changeableCurrencyCode"
+              value={itemByChangeableCurrencyCode.code}
+              onChange={handleChangeableCurrencyCodeChange}
+            >
+              {list.map(({ code }) => (
+                <option key={code}>{code}</option>
+              ))}
+            </Input>
+          </FormGroup>
+        </div>
+
+        <div className="ContentConverter-gridAction">
+          <button
+            className="ContentConverter-gridActionButton"
+            onClick={swapCode}
+          >
+            <img
+              src={imgAction}
+              alt="ContentConverter-imgAction"
+              className="ContentConverter-image"
+            />
+          </button>
+        </div>
+
+        <div className="ContentConverter-gridInputItem">
+          <FormGroup>
+            <Label for="selectCurrency">Получаю</Label>
+            <Input
+              type="number"
+              name="number"
+              id="selectCurrency"
+              value={getSelectCurrencyValue()}
+              disabled
+            />
+          </FormGroup>
+        </div>
+
+        <div className="ContentConverter-gridSelectItem">
+          <FormGroup>
+            <Label for="selcetCurrencyCode">Валюта</Label>
+            <Input
+              type="select"
+              name="select"
+              id="selcetCurrencyCode"
+              value={itemBySelectCurrencyCode.code}
+              onChange={handleSelectCurrencyCodeCodeChange}
+            >
+              {list.map(({ code }) => (
+                <option key={code}>{code}</option>
+              ))}
+            </Input>
+          </FormGroup>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ContentConverter;
--- src/ducks/converter.js
+++ src/ducks/converter.js
@@ -1,15 +1,47 @@
-import { createAction } from "../utils";
+import { createAction, readFromLocalStorige, writeToLocalStorige } from "../utils";
 import api from '../api';
+
+const DEFAULT_CHANGEABLE_CURRENCY_CODE = 'UAH';
+const DEFAULT_SELECT_CURRENCY_CODE = 'USD';
 
 // Actions
 
 const UPDATE = "CONVERTER/UPDATE";
 const START_LOADING = "CONVERTER/START_LOADING";
+const SET_CHANGEABLE_CURRENCY_CODE = "CONVERTER/SET_CHANGEABLE_CURRENCY_CODE";
+const SET_SELECT_CURRENCY_CODE = "CONVERTER/SET_SELECT_CURRENCY_CODE";
+const SWAP_CURRENCY_CODE = "CONVERTER/SWAP_CURRENCY_CODE";
 
 
 export const startLoading = () => createAction(START_LOADING);
 
-export const update = (currencyList) => createAction(UPDATE, { currencyList });
+const update = currencyList => createAction(UPDATE, { currencyList });
+
+export const setChangeableCurrencyCode = code => {
+    writeToLocalStorige('changeable_currency_code', code);
+
+    return createAction(SET_CHANGEABLE_CURRENCY_CODE, { changeableCurrencyCode: code })
+};
+
+export const setSelectCurrencyCode = code => {
+    writeToLocalStorige('select_currency_code', code);
+
+    return createAction(SET_SELECT_CURRENCY_CODE, { selectCurrencyCode: code })
+};
+
+const swapCarrencyCode = () => createAction(SWAP_CURRENCY_CODE);
+
+export const swapCurrencyCodeCallback = () => (dispatch, getState) => {
+    const { converter } = getState();
+
+    const { changeableCurrencyCode, selectCurrencyCode } = converter;
+
+    writeToLocalStorige('changeable_currency_code', selectCurrencyCode);
+
+    writeToLocalStorige('select_currency_code', changeableCurrencyCode);
+
+    dispatch(swapCarrencyCode());
+};
 
 export const request = () => dispatch => {
     dispatch(startLoading());
@@ -35,6 +67,8 @@ const updateListByUah = list => [{ code: 'UAH', rate: 1 }, ...list]
 const initialState = {
     list: [],
     isLoading: false,
+    changeableCurrencyCode: readFromLocalStorige('changeable_currency_code') || DEFAULT_CHANGEABLE_CURRENCY_CODE,
+    selectCurrencyCode: readFromLocalStorige('select_currency_code') || DEFAULT_SELECT_CURRENCY_CODE
 }
 
 const reducer = (state = initialState, { type, payload }) => {
@@ -51,8 +85,39 @@ const reducer = (state = initialState, { type, payload }) => {
             const { currencyList } = payload;
 
             return {
+                ...state,
                 isLoading: false,
                 list: updateListByUah(normlizeList(currencyList))
+            };
+        }
+
+        case SET_CHANGEABLE_CURRENCY_CODE: {
+            const { changeableCurrencyCode } = payload;
+
+            return {
+                ...state,
+                changeableCurrencyCode
+            };
+        }
+
+        case SET_SELECT_CURRENCY_CODE: {
+            const { selectCurrencyCode } = payload;
+
+            return {
+                ...state,
+                selectCurrencyCode
+            };
+        }
+
+        case SWAP_CURRENCY_CODE: {
+
+            const _changeableCurrencyCode = state.changeableCurrencyCode;
+            const _selectCurrencyCode = state.selectCurrencyCode;
+
+            return {
+                ...state,
+                changeableCurrencyCode: _selectCurrencyCode,
+                selectCurrencyCode: _changeableCurrencyCode
             };
         }
 
@@ -63,20 +128,46 @@ const reducer = (state = initialState, { type, payload }) => {
 
 // SELECTORS
 
-export const getList = state => {
-    if (!(state?.converter?.list)) {
+export const getList = state => state.converter.list
+
+export const getIsLoading = state => state.converter.isLoading
+
+export const getChangeableCurrencyCode = state => state.converter.changeableCurrencyCode
+
+export const getSelectCurrencyCode = state => state.converter.selectCurrencyCode
+
+export const getItemByChangeableCurrencyCode = state => {
+    const list = getList(state);
+    const code = getChangeableCurrencyCode(state);
+
+    if (list.length < 2) {
         return null;
     }
 
-    return state.converter.list;
+    const searchItem = list.find(item => item.code === code);
+
+    if (!searchItem) {
+        return list[0];
+    }
+
+    return searchItem;
 }
 
-export const getIsLoading = state => {
-    if (!(state?.converter?.isLoading)) {
+export const getItemBySelectCurrencyCode = state => {
+    const list = getList(state);
+    const code = getSelectCurrencyCode(state);
+
+    if (list.length < 2) {
         return null;
     }
 
-    return state.converter.isLoading;
+    const searchItem = list.find(item => item.code === code);
+
+    if (!searchItem) {
+        return list[1];
+    }
+
+    return searchItem;
 }
 
 export default reducer;
